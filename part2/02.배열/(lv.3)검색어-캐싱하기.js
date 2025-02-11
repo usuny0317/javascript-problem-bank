d3f623b746df87fe59d9cc8c0710701c1f2e1f46
usuny0317 <usuny0317@naver.com>
2025-02-11 11:52:13 +0900

    reduce, 객체 정렬

--- part2/02.배열/(lv.3)검색어-캐싱하기.js
+++ part2/02.배열/(lv.3)검색어-캐싱하기.js
@@ -17,11 +17,32 @@ let topKeywordsCache = [];
 
 function updateTopKeywords(keywords) {
   // TODO
+  // count 객체 생성
+  const count = keywords.reduce((acc, keyword) => {
+    acc[keyword] = (acc[keyword] || 0) + 1;
+    return acc;
+  }, {});
+  // count 를 값기준으로 정렬
+  // 1. 객체라서 바로 sort 못함 배열 만들어야 함.
+  const arrKeywords = Object.entries(count);
+  // 2. 값 기준 내림차순, 값이니까 첫번째 인자.
+  const sortKeywords = arrKeywords.sort((a, b) => b[1] - a[1]);
+  // 3. 키워드 들만 주기 위해서 다시 배열로
+  const arrsort = sortKeywords.map((key) => key[0]);
+
+  //상위 10개 구하기
+  // 10개보다 작을 때 전부 가져오기기
+  if (arrsort.length < 10) {
+    topKeywordsCache = arrsort.slice(0, arrsort.length);
+  } else {
+    //10개보다 크면 잘라서 가져오기기
+    topKeywordsCache = arrsort.slice(0, 10);
+  }
 }
 
 function getTopKeywords() {
   // TODO
-  return [];
+  return topKeywordsCache;
 }
 
 // export를 수정하지 마세요.
