62fa136584b48eaff9584cdcd3c77203ad7bcc54
usuny0317 <usuny0317@naver.com>
2025-02-12 11:14:11 +0900

    배열 객체: reduce 사용, sort 사용

--- part2/02.배열/(lv.3)나이-통계-구하기.js
+++ part2/02.배열/(lv.3)나이-통계-구하기.js
@@ -10,7 +10,18 @@
  * @returns {{ averageAge: number, maxAge: number }}
  */
 
-function calculateStatistics(users) {}
+function calculateStatistics(users) {
+  //0일 때 정해진 값 주기
+  if (users.length === 0) return { averageAge: 0, maxAge: 0 };
+  //객체 배열이니까 배열 메소드 쓸 수 있음
+  //Max부터 sort 한 다음에 젤 앞에 꺼 가져오기
+  const maxAge = users.sort((a, b) => b.age - a.age)[0].age;
+  //평균 foreach가 나을까? age를 누적해야 하니까 reduce를 사용하자!!
+  const totalAge = users.reduce((sum, user) => sum + user.age, 0);
+  const averageAge = totalAge / users.length;
+
+  return { averageAge, maxAge };
+}
 
 // export 를 수정하지 마세요.
 export { calculateStatistics };
