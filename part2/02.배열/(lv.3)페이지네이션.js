e6ef2a8a32d79702ad611fa584b5edf01e5c0814
usuny0317 <usuny0317@naver.com>
2025-02-14 12:33:38 +0900

    페이지네이션: 한 페이지당 나오는 내용 지정, 페이지 나타낼 수 있는 내용 지정하기 sort/ ceil 사용했음

--- part2/02.배열/(lv.3)페이지네이션.js
+++ part2/02.배열/(lv.3)페이지네이션.js
@@ -18,8 +18,25 @@ const posts = [
   { id: 4, title: "Paging" },
   { id: 5, title: "Example" },
 ];
+//perpage: 한 페이지당 나오는 수
+//pagenumber: 페이지
+//그러니까 perpage가 2라면 한 페이지당 2개씩 나타나서 12/ 34/ 5가 된다는 것임
+// perpage가 3이라면 한 페이지당 3개씩 해서 123/ 45 된다는 것!
 
-function getPage(pageNumber, perPage) {}
+function getPage(pageNumber, perPage) {
+  if (perPage < 1 || pageNumber < 1) {
+    return [];
+  } else if (pageNumber > Math.ceil(posts.length / perPage)) {
+    //올림함!!
+    return [];
+  }
+  const start = (pageNumber - 1) * perPage;
+  const end = start + perPage;
+  if (end > posts.length) {
+    return posts.slice(start, posts.length);
+  }
+  return posts.slice(start, end);
+}
 
 // export 를 수정하지 마세요.
 export { getPage, posts };
