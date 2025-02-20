4fe940b2bf4fdf998804d6f89ea775a8649daa9e
usuny0317 <usuny0317@naver.com>
2025-02-20 09:25:02 +0900

    folder search/ 재귀적으로 돌면서 폴더 탐색// 생각보다 까다로움

--- part2/02.배열/(lv.3)폴더-탐색.js
+++ part2/02.배열/(lv.3)폴더-탐색.js
@@ -18,7 +18,21 @@
  */
 
 // TODO: getAllFolderNames 함수를 작성하세요.
-function getAllFolderNames(folder) {}
+function getAllFolderNames(folder) {
+  let result = [];
+  //재귀적으로 돌면서 키값이 name인 것의 values를 뽑아오면 되겠는데
+  //깊은 복사랑 비슷할 것 같은데 어떻게 하더라..
+  //길이를 재, 길이 만큼 name을 먼저 저장하고, 길이만큼 돌면서 subFolder를 봐 얘도 길이를 봐, 없으면 끝! 있으면 앞에 반복!
+  function insideFolder(folder) {
+    result.push(folder.name);
+    for (let sub of folder.subFolders) {
+      insideFolder(sub);
+    }
+  }
+
+  insideFolder(folder);
+  return result;
+}
 
 // export 를 수정하지 마세요.
 export { getAllFolderNames };
