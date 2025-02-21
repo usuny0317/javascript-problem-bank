e914817dd2231e01abbf35267353c4035a4d1f05
usuny0317 <usuny0317@naver.com>
2025-02-21 10:04:29 +0900

    chunk/ 2차 배열로 만들기 undefined에 푸시 막는 법을 배웠다.

--- part2/02.배열/(lv.3)chunk.js
+++ part2/02.배열/(lv.3)chunk.js
@@ -10,7 +10,21 @@
  * @returns {any[][]}
  */
 
-function chunkArray(arr, size) {}
+function chunkArray(arr, size) {
+  // 2차원 배열로... 그니까 arr이 있고 size가 주어지면 size만큼 자른다는 거지..?
+  // 페이지네이션이랑 비슷한거 아닌지..
+  // 그럴려면 arr size로 자르고, 다음 arr[size]를 arr[size+size]로 자르고.. 반복하다가
+  let newarr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let row = Math.floor(i / size);
+    if (!newarr[row]) {
+      //undefinded.push를 막음
+      newarr[row] = [];
+    }
+    newarr[row].push(arr[i]);
+  }
+  return newarr;
+}
 
 // export 를 수정하지 마세요.
 export { chunkArray };
